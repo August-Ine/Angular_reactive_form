cdfea7a14f0ce80f7a9e18a8cb878cfe667be4b3
CodeSandbox <support@codesandbox.io>
2023-08-21 08:57:53 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  Validators,
+  FormControl,
+  FormBuilder,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CodeSandbox';
+  pjStatuses = ['Stable', 'Critical', 'Finished'];
+  projectForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+    this.projectForm = this.fb.group({
+      projectName: [null, [Validators.required], [this.forbiddenNameAsync]],
+      email: [null, [Validators.required, Validators.email]],
+      status: [null, Validators.required],
+    });
+  }
+
+  // ngOnInit() {
+  //   this.projectForm = new FormGroup({
+  //     projectName: new FormControl(null, [
+  //       Validators.required,
+  //     ], [this.forbiddenNameAsync]),
+  //     email: new FormControl(null, [Validators.required, Validators.email]),
+  //     status: new FormControl(null, Validators.required),
+  //   });
+  // }
+
+  // forbiddenName(control: FormControl): { [s: string]: boolean } {
+  //   if (control.value === 'Test') {
+  //     return { nameIsForbidden: true };
+  //   } else {
+  //     return undefined!;
+  //   }
+  // }
+
+  forbiddenNameAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({ nameIsForbidden: true });
+        } else {
+          resolve(undefined);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
 }
